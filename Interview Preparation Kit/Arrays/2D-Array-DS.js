f1d4770f91111494ee1ea8bf1ab379d46b47f6d8
Wilson López Romero <wilson.lopez.romero@icloud.com>
2020-01-12 23:22:21 -0300

    Prettier

--- Interview Preparation Kit/Arrays/2D-Array-DS.js
+++ Interview Preparation Kit/Arrays/2D-Array-DS.js
@@ -1,18 +1,15 @@
 function hourglassSum(arr) {
-  let result = [];
+  let result = []
   arr.forEach((x, index_x, array_x) => {
-    if (index_x > 3) return;
+    if (index_x > 3) return
     x.forEach((y, index_y, array_y) => {
-      if (index_y > 3) return;
-      let acu = 0;
-      acu += array_y[index_y] + array_y[index_y + 1] + array_y[index_y + 2];
-      acu += array_x[index_x + 1][index_y + 1];
-      acu +=
-        array_x[index_x + 2][index_y] +
-        array_x[index_x + 2][index_y + 1] +
-        array_x[index_x + 2][index_y + 2];
-      result.push(acu);
-    });
-  });
-  return Math.max(...result);
+      if (index_y > 3) return
+      let acu = 0
+      acu += array_y[index_y] + array_y[index_y + 1] + array_y[index_y + 2]
+      acu += array_x[index_x + 1][index_y + 1]
+      acu += array_x[index_x + 2][index_y] + array_x[index_x + 2][index_y + 1] + array_x[index_x + 2][index_y + 2]
+      result.push(acu)
+    })
+  })
+  return Math.max(...result)
 }
--- Interview Preparation Kit/Warm-up Challenges/Sock-Merchant.js
+++ Interview Preparation Kit/Warm-up Challenges/Sock-Merchant.js
@@ -1,12 +1,12 @@
 function sockMerchant(n, ar) {
-  let pairs = 0;
-  let color = new Array();
+  let pairs = 0
+  let color = new Array()
   ar.forEach(i => {
-    if (!color[i]) color[i] = i;
+    if (!color[i]) color[i] = i
     else {
-      pairs++;
-      delete color[i];
+      pairs++
+      delete color[i]
     }
-  });
-  return pairs;
+  })
+  return pairs
 }
